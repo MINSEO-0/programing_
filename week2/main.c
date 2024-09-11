cbf251fde2dd5e431edc4aa331bf1fc25ad5c283
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-09-11 14:30:20 +0900

    week2 - 실습

--- week2/main.c
+++ week2/main.c
@@ -7,6 +7,15 @@ int main(int argc, char *argv[]) {
 	
 	printf("Hello, Sookmyung Electronics!\n");
 	
+	int a; //첫번째 정수를 저장할 변수 
+	int b; // 두 번째 정수를 저장할 변수
+	
+	a=10;
+	b=20;
+	
+	printf("%d + %d = %d\n", a, b, a+b);
+ 
+	
 	return 0;
 
 
