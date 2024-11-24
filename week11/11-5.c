33fef32e9cab743d72abdd80d1e6522d2889c634
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-11-24 23:01:32 +0900

    week11

--- week11/11-5.c
+++ week11/11-5.c
@@ -11,15 +11,15 @@ int main(void){
    char word1[50], word2[50], word3[50];
    
     printf("input a word: ");
-    scanf("%s", &word1);
+    scanf("%s", word1);
  
     
     printf("input a word: ");
-    scanf("%s", &word2);
+    scanf("%s", word2);
 
     
     printf("input a word: ");
-    scanf("%s", &word3);
+    scanf("%s", word3);
 
 
     fprintf(fp, "%s\n%s\n%s\n", word1, word2, word3);
